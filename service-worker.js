df9ba42c7d062f6f65af968b24eeb5162095aff7
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-01-22 13:13:25 -0500

    fix service-worker path

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = "to-do-pwa-cache-v1";
 const FILES_TO_CACHE = [
   "./",
-  ".//index.html",
-  ".//style.css",
-  ".//app.js",
-  ".//manifest.json",
-  ".//icons/icon-128.png",
-  ".//icons/icon-512.png",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-128.png",
+  "./icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
