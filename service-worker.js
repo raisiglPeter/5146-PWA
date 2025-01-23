c084a8af2198a8d7ebb2b7bf4be435366af49c4c
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-01-23 10:15:09 -0500

    add log to service-worker

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,11 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(FILES_TO_CACHE).then(() => {
+        console.log("Caching files:", FILES_TO_CACHE);
+      });
+    })
   );
 });
 
