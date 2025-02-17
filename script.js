766cfd4c08549ec0ab0deb6e06aca985b1fab401
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-02-17 11:13:21 -0500

    add firebase, parcel

--- script.js
+++ script.js
@@ -56,6 +56,41 @@ const recipeList = [
   },
 ];
 
+// firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCdNAkePR48BW0AdSOONp7sOfIirYdRv0",
+  authDomain: "pwa-5719b.firebaseapp.com",
+  projectId: "pwa-5719b",
+  storageBucket: "pwa-5719b.firebasestorage.app",
+  messagingSenderId: "1077986382281",
+  appId: "1:1077986382281:web:f1cca2531b3b05ac199818",
+  measurementId: "G-1NPPL3P4HF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// service worker
+const sw = new URL("service-worker.js", import.meta.url);
+if ("serviceWorker" in navigator) {
+  const s = navigator.serviceWorker;
+  s.register(sw.href, {
+    scope: "/5146-PWA/",
+  })
+    .then(() =>
+      console.log(
+        "Service Worker Registered for scope:",
+        sw.href,
+        "with",
+        import.meta.url
+      )
+    )
+    .catch((err) => console.error("Service Worker Error:", err));
+}
+
 // adding HTML elements
 document.addEventListener("DOMContentLoaded", () => {
   const recipeListDiv = document.querySelector(".recipe-list");
