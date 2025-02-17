08386703f8a787a1faa2beec9ddafbc275729686
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-02-17 10:55:25 -0500

    fix service worker path

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
   "/5146-PWA/",
   "/5146-PWA/index.html",
   "/5146-PWA/style.css",
-  "/5146-PWA/app.js",
+  "/5146-PWA/script.js",
   "/5146-PWA/manifest.json",
   "/5146-PWA/icons/icon-128.png",
   "/5146-PWA/icons/icon-512.png",
@@ -11,22 +11,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        console.log("Opened cache:", CACHE_NAME);
-        cache
-          .addAll(FILES_TO_CACHE)
-          .then(() => {
-            console.log("Caching files:", FILES_TO_CACHE);
-          })
-          .catch((error) => {
-            console.error("Failed to cache files:", error);
-          });
-      })
-      .catch((error) => {
-        console.error("Failed to open cache:", error);
-      })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
