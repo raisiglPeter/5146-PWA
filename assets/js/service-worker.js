7b9dc3cce5f79ead30090834a9414c2aaf464179
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-02-19 16:17:32 -0500

    fix path in FILES_TO_CACHE

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,18 +1,16 @@
 const CACHE_NAME = "recipe-list-cache-v1";
 const FILES_TO_CACHE = [
   "/5146-PWA/",
-  "/5146-PWA/html/index.html",
-  "/5146-PWA/html/recipes.html",
-  "/5146-PWA/assets/css/style.css",
+  "/5146-PWA/assets/html/index.html",
+  "/5146-PWA/assets/html/recipes.html",
   "/5146-PWA/assets/css/style-sign-in.css",
-  "/5146-PWA/js/firebase.js",
-  "/5146-PWA/js/recipes.js",
-  "/5146-PWA/js/signIn.js",
-  "/5146-PWA/js/service-worker.js",
+  "/5146-PWA/assets/css/style.css",
+  "/5146-PWA/assets/js/firebase.js",
+  "/5146-PWA/assets/js/script.js",
+  "/5146-PWA/assets/js/signIn.js",
+  "/5146-PWA/assets/icons/icon-128.png",
+  "/5146-PWA/assets/icons/icon-512.png",
   "/5146-PWA/manifest.json",
-  "/5146-PWA/icons/favicon.ico",
-  "/5146-PWA/icons/icon-128.png",
-  "/5146-PWA/icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
