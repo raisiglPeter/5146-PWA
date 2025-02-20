63f5229f451a24e58b226b00b8407b68623917c2
raisiglPeter <p_raisigl@fanshaweonline.ca>
2025-02-20 09:20:27 -0500

    delete service worker

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -4,22 +4,6 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 const provider = new GoogleAuthProvider();
 const signInBttn = document.getElementById("signInButton");
 
-const sw = new URL("service-worker.js", import.meta.url);
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register(sw.href, { scope: "/5146-PWA/" })
-    .then(() =>
-      console.log(
-        "Service Worker Registered for scope:",
-        sw.href,
-        "with",
-        import.meta.url
-      )
-    )
-    .catch((err) => console.error("Service Worker Error:", err));
-}
-
 function signIn() {
   signInWithPopup(auth, provider)
     .then((result) => {
